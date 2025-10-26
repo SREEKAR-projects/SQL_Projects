df6a0f0e0ba8924df41a568816cb9c21090a2a37
SREEKAR-projects <sreekarrv21@gmail.com>
2025-10-26 23:42:29 +0530

    Updated Project with fixes and new challanges

--- RESTRAUNTS_PROJECT.sql
+++ RESTRAUNTS_PROJECT.sql
@@ -1,3 +1,25 @@
+/*
+OBJECTIVE BRIEFING: EXPLORE THE MENU ITEMS TABLE (EXPLORATORY)
+
+The objective for the Taste of the World Cafe project was to explore the `menu items` table (also referred to as the `items` table) to get an initial understanding of the restaurant's new menu offerings.
+https://www.youtube.com/watch?v=JaUKDbCXMX4
+1.  Table Overview and Item Count:
+    *   View the items table.
+    *   Write a query to find the total number of items on the menu.
+
+2.  Overall Price Extremes:
+    *   What are the least and most expensive items on the menu?
+
+3.  Italian Dish Analysis:
+    *   How many Italian dishes are on the menu?
+    *   What are the **least and most expensive Italian dishes on the menu?
+
+4.  Category Pricing and Counts:
+    *   Take a look at how many **dishes are in each category.
+    *   What is the average dish price within each category?
+
+*/
+
 select count(menu_item_id) as total_iteams
 from menu_items;
 
