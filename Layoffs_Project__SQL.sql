3a31d3ffc6879d75a860ccf37381b0050611cebb
SREEKAR-projects <sreekarrv21@gmail.com>
2025-10-28 15:35:44 +0530

    Update with my local changes

--- Layoffs_Project__SQL.sql
+++ Layoffs_Project__SQL.sql
@@ -126,7 +126,11 @@ where total_laid_off is null and percentage_laid_off is null;
 
 select*
 from layoffs_staging2
-where total_laid_off is null and percentage_laid_off is null
+where total_laid_off is null and percentage_laid_off is null;
+
+alter table layoffs_staging2
+drop column row_num;
 
 
-
+select*
+from layoffs_staging2
